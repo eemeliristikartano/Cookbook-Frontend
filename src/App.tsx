6d80bdd8935eebde577c702a1e3546ec60ef6a55
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-02 13:18:32 +0200

    Added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css';
 import Recipelist from './components/Recipelist';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { useState } from 'react';
+import Instructions from './components/Instructions';
 
 function App() {
+  const [value, setValue] = useState<number>(0);
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => setValue(newValue);
+
 
   return (
     <div className="App">
-      <Recipelist />
+      <Tabs value={value} onChange={handleTabChange}>
+        <Tab value={0} label='Recipes' />
+        <Tab value={1} label='Instructions' />
+      </Tabs>
+      {value === 0 && <Recipelist />}
+      {value === 1 && <Instructions />}
     </div>
   );
 }
