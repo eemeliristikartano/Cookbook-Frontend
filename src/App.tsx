d261170aaccac5152deab5ac46bb35b9c0489b3a
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-03 19:46:42 +0200

    New tab for navigation and state for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,29 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { useState } from 'react';
 import Instructions from './components/Instructions';
+import Login from './components/Login';
+import { Button, Stack } from "@mui/material";
+import UserRecipes from './components/UserRecipes';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // For navigation.
   const [value, setValue] = useState<number>(0);
-
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => setValue(newValue);
-
+  const handleIsAuthenticated = () => setIsAuthenticated(!isAuthenticated);
 
   return (
     <div className="App">
+      {/* If the user is not authenticated there is Log in- button. Else, there is Log out- button */}
+      {!isAuthenticated ? <Login handleIsAuthenticated={handleIsAuthenticated} /> : <Button variant="contained" sx={{ display: 'flex' }} onClick={handleIsAuthenticated} >Log out</Button>}
       <Tabs value={value} onChange={handleTabChange}>
         <Tab value={0} label='Recipes' />
-        <Tab value={1} label='Instructions' />
+        <Tab value={1} label="User's recipes" disabled={!isAuthenticated} />
+        <Tab value={2} label="Instructions" />
       </Tabs>
-      {value === 0 && <Recipelist />}
-      {value === 1 && <Instructions />}
+      {value === 0 && <Recipelist isAuthenticated={isAuthenticated} />}
+      {value === 1 && <UserRecipes isAuthenticated={isAuthenticated} />}
+      {value === 2 && <Instructions />}
     </div>
   );
 }
