31a4924ae4bad0ac899ee1815e5be81e3ca47a08
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-04 13:54:59 +0200

    Added comments, added new function that handles logging out

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,21 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   // For navigation.
   const [value, setValue] = useState<number>(0);
+  // Handles tab changes.
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => setValue(newValue);
+  // Set isAuthenticated true.
   const handleIsAuthenticated = () => setIsAuthenticated(!isAuthenticated);
+  const handleLogOut = () => {
+    // Remove token from session storage.
+    sessionStorage.removeItem('jwt-token');
+    // Set isAuthenticated false.
+    setIsAuthenticated(!isAuthenticated);
+  }
 
   return (
     <div className="App">
-      {/* If the user is not authenticated there is Log in- button. Else, there is Log out- button */}
-      {!isAuthenticated ? <div style={{ display: 'flex', flexDirection: 'row' }} > <Login handleIsAuthenticated={handleIsAuthenticated} /> <Register /> </div> : <Button variant="contained" sx={{ display: 'flex' }} onClick={handleIsAuthenticated} >Log out</Button>}
+      {/* If the user is not authenticated there is Log in-button and Register-button. Else, there is Log out- button */}
+      {!isAuthenticated ? <div style={{ display: 'flex', flexDirection: 'row' }} > <Login handleIsAuthenticated={handleIsAuthenticated} /> <Register /> </div> : <Button variant="contained" sx={{ display: 'flex' }} onClick={handleLogOut} >Log out</Button>}
       <Tabs value={value} onChange={handleTabChange}>
         <Tab value={0} label='Recipes' />
         <Tab value={1} label="My recipes" disabled={!isAuthenticated} />
