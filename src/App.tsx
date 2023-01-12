6932d5b74265f32e054e22cf56887a36290df41b
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-12 18:18:08 +0200

    Renders ControlButtons component, grid for tabs

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { useState } from 'react';
 import Instructions from './components/Instructions';
-import Login from './components/Login';
-import { Button } from "@mui/material";
 import UserRecipes from './components/UserRecipes';
-import Register from './components/Register';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import ControlButtons from './components/ControlButtons';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -24,18 +23,24 @@ function App() {
     setIsAuthenticated(!isAuthenticated);
   }
 
+  // It the user is on 'MY RECIPES' tab and logs out --> move to the 'RECIPES' tab.
+  if (sessionStorage.getItem('jwt-token') === null && value === 1) setValue(0);
+
   return (
     <div className="App">
       {/* If the user is not authenticated there is Log in-button and Register-button. Else, there is Log out- button */}
-      {!isAuthenticated ? <div style={{ display: 'flex', flexDirection: 'row' }} > <Login handleIsAuthenticated={handleIsAuthenticated} /> <Register /> </div> : <Button variant="contained" sx={{ display: 'flex' }} onClick={handleLogOut} >Log out</Button>}
-      <Tabs value={value} onChange={handleTabChange}>
+      <Tabs value={value} onChange={handleTabChange} variant='fullWidth' >
         <Tab value={0} label='Recipes' />
-        <Tab value={1} label="My recipes" disabled={!isAuthenticated} />
+        <Tab value={1} label="My recipes" disabled={sessionStorage.getItem('jwt-token') === null} />
         <Tab value={2} label="Instructions" />
       </Tabs>
-      {value === 0 && <Recipelist isAuthenticated={isAuthenticated} />}
-      {value === 1 && <UserRecipes isAuthenticated={isAuthenticated} />}
-      {value === 2 && <Instructions />}
+      <Grid2 container margin={3}   >
+        {value === 0 && <Recipelist />}
+        {value === 1 && <UserRecipes />}
+        {value === 2 && <Instructions />}
+      </Grid2>
+      {/* Buttons for logging in, register and logging out.  */}
+      <ControlButtons handleIsAuthenticated={handleIsAuthenticated} handleLogOut={handleLogOut} />
     </div>
   );
 }
